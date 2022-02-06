980876e040f9fef6d18228b0ebe82f90c94a381d
Caitlyn Griffing <caitlyn.griffing@gmail.com>
2022-02-06 15:51:08 -0600

    added current date

--- assets/sandbox.js
+++ assets/sandbox.js
@@ -1,2 +1,18 @@
-const key = "8f9a11f5149a0f8020ecd4c8516f848a";
+// add city, country code; api key; unit of temp (imperial)
 
+// My API key: 8f9a11f5149a0f8020ecd4c8516f848a
+var myKey = "8f9a11f5149a0f8020ecd4c8516f848a";
+
+// define and display current date
+// var moment = require('moment');
+
+
+
+var currentDate = $(".current-date");
+
+function displayDate() {
+    var current = moment().format('ll');
+    currentDate.text(current);
+}
+
+displayDate();
